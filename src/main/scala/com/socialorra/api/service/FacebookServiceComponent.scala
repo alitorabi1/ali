e10f17d6a6768c3377523afd5f39b986d36786c8
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2014-06-13 20:58:18 -0400

    FB and Twitter APIs are async now.

--- src/main/scala/com/socialorra/api/service/FacebookServiceComponent.scala
+++ src/main/scala/com/socialorra/api/service/FacebookServiceComponent.scala
@@ -1,6 +1,8 @@
 package com.socialorra.api.service
 
 import com.socialorra.api.repo.facebook.{AccessToken, FacebookRepositoryComponent}
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
 /**
  * Facebook services.
@@ -16,8 +18,8 @@ trait FacebookServiceComponent {
   // Facebook services implement this trait
   trait FacebookService {
     def getOAuthAuthorizationURL(callbackURL: String) : String
-    def getOAuthAccessToken(oauthCode: String, callbackURL: String) : AccessToken
-    def getName(implicit accessToken: AccessToken) : String
+    def getOAuthAccessToken(oauthCode: String, callbackURL: String) : Future[AccessToken]
+    def getName(implicit accessToken: AccessToken) : Future[String]
   }
 
   // Facebook service that sends facebooks using injected facebook repository
@@ -26,11 +28,11 @@ trait FacebookServiceComponent {
       facebookRepository.getOAuthAuthorizationURL(callbackURL)
     }
 
-    def getOAuthAccessToken(oauthCode: String, callbackURL: String) : AccessToken = {
+    def getOAuthAccessToken(oauthCode: String, callbackURL: String) : Future[AccessToken] = future {
       facebookRepository.getOAuthAccessToken(oauthCode, callbackURL)
     }
 
-    def getName(implicit accessToken: AccessToken) : String = {
+    def getName(implicit accessToken: AccessToken) : Future[String] = future {
       facebookRepository.getName(accessToken)
     }
   }
--- src/main/scala/com/socialorra/api/service/TwitterServiceComponent.scala
+++ src/main/scala/com/socialorra/api/service/TwitterServiceComponent.scala
@@ -1,5 +1,7 @@
 package com.socialorra.api.service
 
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 import com.socialorra.api.repo.twitter.{AccessToken, RequestToken, TwitterRepositoryComponent}
 
 /**
@@ -15,22 +17,22 @@ trait TwitterServiceComponent {
 
   // Twitter services implement this trait
   trait TwitterService {
-    def getOAuthAccessToken(requestToken: RequestToken, oauthVerifier: String) : AccessToken
-    def getOAuthRequestToken(callbackURL: String) : RequestToken
-    def getScreenName(implicit accessToken: AccessToken) : String
+    def getOAuthAccessToken(requestToken: RequestToken, oauthVerifier: String) : Future[AccessToken]
+    def getOAuthRequestToken(callbackURL: String) : Future[RequestToken]
+    def getScreenName(implicit accessToken: AccessToken) : Future[String]
   }
 
   // Twitter service that communicates with Twitter via injected twitter repository
   class TwitterServiceImpl extends TwitterService {
-    def getOAuthAccessToken(requestToken: RequestToken, oauthVerifier: String) : AccessToken = {
+    def getOAuthAccessToken(requestToken: RequestToken, oauthVerifier: String) : Future[AccessToken] = future {
       twitterRepository.getOAuthAccessToken(requestToken, oauthVerifier)
     }
 
-    def getOAuthRequestToken(callbackURL: String) : RequestToken = {
+    def getOAuthRequestToken(callbackURL: String) : Future[RequestToken] = future {
       twitterRepository.getOAuthRequestToken(callbackURL)
     }
 
-    def getScreenName(implicit accessToken: AccessToken) : String = {
+    def getScreenName(implicit accessToken: AccessToken) : Future[String] = future {
       twitterRepository.getScreenName(accessToken)
     }
   }
