9b9286f76eefa1ca12dfd3f91429cfc45c9f8d85
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2014-06-21 17:43:30 -0400

    The EmailRepositoryImpl now prefixes it's config options with "so-api". The configuration file allows for "application.overrides.conf" inclusion.

--- src/main/scala/com/socialorra/api/repo/email/EmailRepositoryComponent.scala
+++ src/main/scala/com/socialorra/api/repo/email/EmailRepositoryComponent.scala
@@ -28,15 +28,15 @@ trait EmailRepositoryComponent {
 
     val conf = ConfigFactory.load()
 
-    val smtpHost = try { conf.getString("smtp.host") } catch { case e: ConfigException.Missing => "localhost" }
-    val smtpPort = try { conf.getInt("smtp.port")    } catch { case e: ConfigException.Missing => 25 }
-    val smtpSsl  = try { conf.getBoolean("smtp.ssl") } catch { case e: ConfigException.Missing => false }
-    val smtpTls  = try { conf.getBoolean("smtp.tls") } catch { case e: ConfigException.Missing => false }
+    val smtpHost = try { conf.getString("so-api.smtp.host") } catch { case e: ConfigException.Missing => "localhost" }
+    val smtpPort = try { conf.getInt("so-api.smtp.port")    } catch { case e: ConfigException.Missing => 25 }
+    val smtpSsl  = try { conf.getBoolean("so-api.smtp.ssl") } catch { case e: ConfigException.Missing => false }
+    val smtpTls  = try { conf.getBoolean("so-api.smtp.tls") } catch { case e: ConfigException.Missing => false }
 
-    val smtpUser     = try { Some(conf.getString("smtp.user"))     } catch { case e: ConfigException.Missing => None }
-    val smtpPassword = try { Some(conf.getString("smtp.password")) } catch { case e: ConfigException.Missing => None }
+    val smtpUser     = try { Some(conf.getString("so-api.smtp.user"))     } catch { case e: ConfigException.Missing => None }
+    val smtpPassword = try { Some(conf.getString("so-api.smtp.password")) } catch { case e: ConfigException.Missing => None }
 
-    val smtpNumSenders = try { conf.getInt("smtp.number-of-senders") } catch { case e: ConfigException.Missing => 1 }
+    val smtpNumSenders = try { conf.getInt("so-api.smtp.number-of-senders") } catch { case e: ConfigException.Missing => 1 }
 
     // TODO: move this into an object of it's own
     val senderProps = Props(classOf[SmtpActor],
